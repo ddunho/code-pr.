d5db0817f3fa47954bdcda38576063cf18ca63c1
ddunho <dydkwogur@naver.com>
2024-12-21 17:09:22 +0900

    first commit

---
+++ 구구단 곱셈표 출력하기.py
@@ -0,0 +1,8 @@
+#구구단 곱셈표 출력하기
+
+print('-' * 27)
+for i in range(1, 10):
+    for j in range(1, 10):
+        print(f'{i * j:3}], end='') #:3 -> 출력할 값이 최소 3칸의 공간을 차지
+              print()
+print('-' * 27)
