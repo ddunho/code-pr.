32c386e87d8c042717a5577d9a1794558eb73ef9
ddunho <dydkwogur@naver.com>
2025-03-22 19:21:18 +0900

    first commit

--- 뮤터블 시퀀스 원소를 역순으로 정렬.py
+++ 뮤터블 시퀀스 원소를 역순으로 정렬.py
@@ -6,4 +6,9 @@ def reverse_array(a: MutableSequence) -> None;
 #뮤터블 시퀀스 a의 원소를 역순으로 정렬
     n = len(a)
     for i in range(n //2):
-        ai], a[n - i - 1] = a[n - i - 1], a[i]
+        a[i], a[n - i - 1] = a[n - i - 1], a[i]
+
+if __name__ == '__main__':
+    print('배열 원소를 역순으로 정렬합니다.')
+    nx = int(intput('원소 수를 입력하세요.:'))
+    x = [None] * nx # 원소 수가 nx인 리스트를 생성성
