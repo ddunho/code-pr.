1a9d1fc5380eab116815f3ce90004e653afaff4b
ddunho <dydkwogur@naver.com>
2025-04-11 11:00:24 +0900

    first commit

--- 1000이하의 소수를 나열하기.py
+++ 1000이하의 소수를 나열하기.py
@@ -6,3 +6,9 @@ counter = 0
 for in range(2, 1001):
     for i in range(2, n):
         counter += 1
+        if n % i ==0:
+            break
+    else:
+        print(n)
+
+print(f'나눗셈을 실행한 횟수: {counter}')
