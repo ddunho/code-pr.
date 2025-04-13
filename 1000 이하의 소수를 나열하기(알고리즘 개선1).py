08a7528e767aa9aa652cc89eddad93f85063b667
ddunho <dydkwogur@naver.com>
2025-04-13 15:53:33 +0900

    first commit

--- 1000 이하의 소수를 나열하기(알고리즘 개선1).py
+++ 1000 이하의 소수를 나열하기(알고리즘 개선1).py
@@ -4,3 +4,13 @@ counter = 0 #나눗셈 횟수를 카운트
 ptr = 0 #이미 찾은 소수의 갯수
 prime = [None] * 500 #소수를 저장하는 배열
 
+
+prime[ptr] = 2  #2는 소수이므로 초깃값으로 지정
+ptr += 1
+
+for n in range(3, 1001, 2):  #홀수만을 대상으로 설정
+    for i in range(1, ptr):
+        counter += 1
+        if n %prime[i] == 0:
+            break
+
