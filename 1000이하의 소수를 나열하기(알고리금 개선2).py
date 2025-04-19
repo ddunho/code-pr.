e48bd2bafd3c188d3cca4955e25e4f24a78769da
ddunho <dydkwogur@naver.com>
2025-04-20 02:46:14 +0900

    first commit

--- 1000이하의 소수를 나열하기(알고리금 개선2).py
+++ 1000이하의 소수를 나열하기(알고리금 개선2).py
@@ -10,3 +10,12 @@ ptr += 1
 
 prime[ptr] = 3
 ptr += 1
+
+for n in range(5, 1001, 2):
+    i = 1
+    while prime[i] * prime[i] <= n:
+        counter += 2
+        if n % prime[i] == 0:
+            break
+        i += 1
+        else:
