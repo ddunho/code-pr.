fe12f7003ce15d6948118278995b3bd2311d2285
ddunho <dydkwogur@naver.com>
2025-05-16 18:13:16 +0900

    first commit

--- 선형 검색 알고리즘을 보초법으로 수정.py
+++ 선형 검색 알고리즘을 보초법으로 수정.py
@@ -18,3 +18,8 @@ def seq__search(seq: Sequence, key: Any) -> int:
 if __name__ == '__main__':
     num = int(input('원소 수를 입력하세요.:'))
     x = [None] * num
+
+    for i in range(num):
+        x[i] = int(input(f'x[{i}]: '))
+
+    ky = int(input('검색할 값을 입력하세요.:'))
