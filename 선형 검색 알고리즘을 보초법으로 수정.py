b490080822a50238d37f81260dbf092e0960fba1
ddunho <dydkwogur@naver.com>
2025-05-17 19:47:43 +0900

    first commit

--- 선형 검색 알고리즘을 보초법으로 수정.py
+++ 선형 검색 알고리즘을 보초법으로 수정.py
@@ -23,3 +23,11 @@ if __name__ == '__main__':
         x[i] = int(input(f'x[{i}]: '))
 
     ky = int(input('검색할 값을 입력하세요.:'))
+
+    idx = seq_search(x, ky)
+
+    if idx == -1:
+        print('검색값을 갖는 원소가 존재하지 않습니다.')
+
+    else:
+        print(f'검색값은 x[{idx}]에 있습니다.')
