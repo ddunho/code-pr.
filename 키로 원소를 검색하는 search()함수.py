f6c6c5c2dbe65a28cac20e34e72a88d77d07d59c
ddunho <dydkwogur@naver.com>
2025-06-20 18:11:16 +0900

    first commit

--- 키로 원소를 검색하는 search()함수.py
+++ 키로 원소를 검색하는 search()함수.py
@@ -1,6 +1,6 @@
 #키로 원소를 검색하는 search()함수
 
-    def search(self, key: Any) -> Any:
+def search(self, key: Any) -> Any:
         hash = self.hash_value(key)
         p = self.table[hash]
 
@@ -10,4 +10,9 @@
             p = p.next
 
         return None
+
+def add(self, key: Any, value: Any) -> bool:
+     hash = self.hash_value(key)
+     p = self.table[hash]
+    
             
