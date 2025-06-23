4b93a179b48dd1ac5086c1d9b551d14211c41bd8
ddunho <dydkwogur@naver.com>
2025-06-24 00:31:37 +0900

    first commit

--- remove() 함수로 원소를 삭제하기.py
+++ remove() 함수로 원소를 삭제하기.py
@@ -4,3 +4,10 @@ def remove(self, key: Any) -> bool:
     hash = self.hash_value(key)
     p = self.table[hash]
     pp = None
+
+    while p is not None:
+        if p.key == key:
+            if pp is None:
+                self.table[hash] = p.next
+            else:
+            
