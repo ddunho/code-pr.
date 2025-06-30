28f80e2df28bb3b19168613777ae88437fda6b81
ddunho <dydkwogur@naver.com>
2025-06-30 18:53:46 +0900

    first commit

--- 체인법을 구현하는 해시 클래스 ChainedHash의 사용.py
+++ 체인법을 구현하는 해시 클래스 ChainedHash의 사용.py
@@ -4,3 +4,9 @@ from enum import Enum
 from chained_hash import ChainedHash
 
 Menu = Enum('Menu', ['추가', '삭제', '검색', '덤프', '종료'])
+
+
+def select_menu() -> Menu:
+    s = [f'({m.value}){m.name}' for m in Menu]
+    while True:
+        print(*s, sep = '    ', end = '')
