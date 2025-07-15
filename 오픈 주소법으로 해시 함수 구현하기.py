81ead6ef0c2229a86b913a4433b8ee972a85b3a4
ddunho <dydkwogur@naver.com>
2025-07-15 17:59:24 +0900

    first commit

--- 오픈 주소법으로 해시 함수 구현하기.py
+++ 오픈 주소법으로 해시 함수 구현하기.py
@@ -4,3 +4,10 @@ from __future__ import annotations
 from typing import Any, Type
 from enum import Enum
 import hashlib
+
+
+#버킷의 속성
+class Status(Enum):
+    OCCUPied = 0
+    EMPTY = 1
+    DELETED = 2
