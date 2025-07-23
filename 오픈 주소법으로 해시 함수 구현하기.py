38e2b07077aa5c2da8af924e6e8b25f107480709
ddunho <dydkwogur@naver.com>
2025-07-24 07:09:24 +0900

    first commit

--- 오픈 주소법으로 해시 함수 구현하기.py
+++ 오픈 주소법으로 해시 함수 구현하기.py
@@ -40,7 +40,8 @@ class OpenHash:
     
     def hash_value(self, key: Any) -> int:
         if isinstance(key, int):
-            return key % self.capacity    
+            return key % self.capacity
+        return(int(hashlib.md5(str(key).encode().hexdigest(). 16) %self.capacity))
     
     def search node(self, key: Any) -> Any:
         hash = self.hash_value(key)
